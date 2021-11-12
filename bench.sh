58daf6a1a3973d51fb860e9b0ec588c376f281fc
Matteo Collina <hello@matteocollina.com>
2021-11-12 12:34:19 +0100

    Run baseline first

--- bench.sh
+++ bench.sh
@@ -1,6 +1,15 @@
 #! /bin/bash
 
 echo '==============================='
+echo '= Gateway Mode (not cache)    ='
+echo '==============================='
+npx concurrently --raw -k \
+  "node ./bench/gateway-service-1.js" \
+  "node ./bench/gateway-service-2.js" \
+  "npx wait-on tcp:3001 tcp:3002 && node ./bench/gateway.js" \
+  "npx wait-on tcp:3000 && node ./bench/gateway-bench.js"
+
+echo '==============================='
 echo '= Gateway Mode (10ms TTL)     ='
 echo '==============================='
 npx concurrently --raw -k \
@@ -25,13 +34,4 @@ npx concurrently --raw -k \
   "node ./bench/gateway-service-1.js" \
   "node ./bench/gateway-service-2.js" \
   "npx wait-on tcp:3001 tcp:3002 && node ./bench/gateway.js 10000" \
-  "npx wait-on tcp:3000 && node ./bench/gateway-bench.js"
-
-echo '==============================='
-echo '= Gateway Mode (not cache)    ='
-echo '==============================='
-npx concurrently --raw -k \
-  "node ./bench/gateway-service-1.js" \
-  "node ./bench/gateway-service-2.js" \
-  "npx wait-on tcp:3001 tcp:3002 && node ./bench/gateway.js" \
   "npx wait-on tcp:3000 && node ./bench/gateway-bench.js"
