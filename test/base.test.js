8ceb7041c17771abb827888eb7f1b20b3b74388a
Matteo Collina <hello@matteocollina.com>
2021-11-16 18:19:43 +0100

    No ttl means no cache (#36)
    * No ttl means no cache
    
    * Added a test for no ttl case

--- test/base.test.js
+++ test/base.test.js
@@ -5,7 +5,9 @@ const fastify = require('fastify')
 const mercurius = require('mercurius')
 const cache = require('..')
 
-const FakeTimers = require('@sinonjs/fake-timers')
+const { promisify } = require('util')
+
+const immediate = promisify(setImmediate)
 
 test('cache a resolver', async ({ equal, same, pass, plan, teardown }) => {
   plan(11)
@@ -38,6 +40,7 @@ test('cache a resolver', async ({ equal, same, pass, plan, teardown }) => {
   let misses = 0
 
   app.register(cache, {
+    ttl: 4242,
     onHit (type, name) {
       equal(type, 'Query')
       equal(name, 'add')
@@ -93,6 +96,83 @@ test('cache a resolver', async ({ equal, same, pass, plan, teardown }) => {
 
   equal(hits, 1)
   equal(misses, 1)
+})
+
+test('No TTL', async ({ equal, same, pass, plan, teardown }) => {
+  plan(13)
+
+  const app = fastify()
+  teardown(app.close.bind(app))
+
+  const schema = `
+    type Query {
+      add(x: Int, y: Int): Int
+      hello: String
+    }
+  `
+
+  const resolvers = {
+    Query: {
+      async add (_, { x, y }) {
+        pass('add called only once')
+        await immediate()
+        return x + y
+      }
+    }
+  }
+
+  app.register(mercurius, {
+    schema,
+    resolvers
+  })
+
+  let hits = 0
+  let misses = 0
+
+  app.register(cache, {
+    onHit (type, name) {
+      equal(type, 'Query', 'on hit')
+      equal(name, 'add')
+      hits++
+    },
+    onMiss (type, name) {
+      equal(type, 'Query', 'on miss')
+      equal(name, 'add')
+      misses++
+    },
+    policy: {
+      Query: {
+        add: true
+      }
+    }
+  })
+
+  await Promise.all([
+    query(),
+    query()
+  ])
+
+  equal(hits, 1)
+  equal(misses, 2)
+
+  async function query () {
+    const query = '{ add(x: 2, y: 2) }'
+
+    const res = await app.inject({
+      method: 'POST',
+      url: '/graphql',
+      body: {
+        query
+      }
+    })
+
+    equal(res.statusCode, 200)
+    same(res.json(), {
+      data: {
+        add: 4
+      }
+    })
+  }
 })
 
 test('cache a nested resolver with loaders', async ({ same, pass, plan, teardown }) => {
@@ -468,99 +548,6 @@ test('skip the cache', async ({ equal, same, pass, plan, teardown }) => {
       headers: {
         Authorization: 'Bearer xyz'
       },
-      body: {
-        query
-      }
-    })
-
-    equal(res.statusCode, 200)
-    same(res.json(), {
-      data: {
-        add: 4
-      }
-    })
-  }
-})
-
-test('external cache', async ({ equal, same, pass, plan, teardown }) => {
-  plan(8)
-
-  const clock = FakeTimers.install({
-    shouldAdvanceTime: true,
-    advanceTimeDelta: 40
-  })
-  teardown(() => clock.uninstall())
-
-  const app = fastify()
-  teardown(app.close.bind(app))
-
-  const schema = `
-    type Query {
-      add(x: Int, y: Int): Int
-      hello: String
-    }
-  `
-
-  const resolvers = {
-    Query: {
-      async add (_, { x, y }) {
-        pass('add called once')
-        return x + y
-      }
-    }
-  }
-
-  app.register(mercurius, {
-    schema,
-    resolvers
-  })
-
-  const map = new Map()
-
-  app.register(cache, {
-    ttl: 1,
-    storage: {
-      async get (key) {
-        pass('get called with ' + key)
-        return map.get(key)
-      },
-      async set (key, value) {
-        pass('set called')
-        map.set(key, value)
-      }
-    },
-    policy: {
-      Query: {
-        add: true
-      }
-    }
-  })
-
-  const query = '{ add(x: 2, y: 2) }'
-
-  {
-    const res = await app.inject({
-      method: 'POST',
-      url: '/graphql',
-      body: {
-        query
-      }
-    })
-
-    equal(res.statusCode, 200)
-    same(res.json(), {
-      data: {
-        add: 4
-      }
-    })
-  }
-
-  await clock.tickAsync(2000)
-
-  {
-    const res = await app.inject({
-      method: 'POST',
-      url: '/graphql',
       body: {
         query
       }
