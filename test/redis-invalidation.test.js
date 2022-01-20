3d7b85e2a8618e1c259efc5c0cd8768f0856570d
Jhon Rocha <jhmrocha@gmail.com>
2022-01-20 13:54:03 -0300

    test: refactor to dry

--- test/redis-invalidation.test.js
+++ test/redis-invalidation.test.js
@@ -18,230 +18,172 @@ beforeEach(async () => {
 })
 
 test('redis invalidation', async () => {
-  const schema = `
-    type Query {
-      get (id: Int): String
-      search (id: Int): String
-    }
-    type Mutation {
-      set (id: Int): String
-    }
-  `
-  const resolvers = {
-    Query: {
-      async get (_, { id }) {
-        return 'get ' + id
-      },
-      async search (_, { id }) {
-        return 'search ' + id
+  const setupServer = ({ onMiss, onHit, invalidate, onError, tap }) => {
+    const schema = `
+      type Query {
+        get (id: Int): String
+        search (id: Int): String
       }
-    },
-    Mutation: {
-      async set (_, { id }) {
-        return 'set ' + id
+      type Mutation {
+        set (id: Int): String
       }
-    }
-  }
-
-  test('should remove storage keys by references', async t => {
-    // Setup Fastify and Mercurius
-    const app = fastify()
-    t.teardown(app.close.bind(app))
-    app.register(mercurius, { schema, resolvers })
-    // Setup Cache
-    let miss = 0
-    app.register(cache, {
-      ttl: 100,
-      storage: {
-        type: 'redis',
-        options: { client: redisClient, invalidation: true }
-      },
-      onMiss () {
-        ++miss
-      },
-      policy: {
-        Query: {
-          get: {
-            references: async ({ arg }) => [`get:${arg.id}`]
-          },
-          search: true
+    `
+    const resolvers = {
+      Query: {
+        async get (_, { id }) {
+          return 'get ' + id
         },
-        Mutation: {
-          set: {
-            invalidate: async (_, arg) => [`get:${arg.id}`]
-          }
+        async search (_, { id }) {
+          return 'search ' + id
+        }
+      },
+      Mutation: {
+        async set (_, { id }) {
+          return 'set ' + id
         }
       }
-    })
-    // Cache the follwoing
-    await request({ app, query: '{ get(id: 11) }' })
-    t.equal(miss, 1)
-    await request({ app, query: '{ get(id: 12) }' })
-    t.equal(miss, 2)
-    await request({ app, query: '{ search(id: 11) }' })
-    t.equal(miss, 3)
-    // Request a mutation
-    await request({ app, query: 'mutation { set(id: 11) }' })
-    t.equal(miss, 3)
-    // 'get:11' should not be present in cache anymore
-    await request({ app, query: '{ get(id: 11) }' })
-    t.equal(miss, 4)
-    await request({ app, query: '{ search(id: 11) }' })
-    t.equal(miss, 4)
-    await request({ app, query: '{ get(id: 12) }' })
-    t.equal(miss, 4)
-  })
-
-  test('should not remove storage key by not existing reference', async t => {
-    // Setup Fastify and Mercurius
+    }
     const app = fastify()
-    t.teardown(app.close.bind(app))
+    tap.teardown(app.close.bind(app))
     app.register(mercurius, { schema, resolvers })
     // Setup Cache
-    let miss = 0
     app.register(cache, {
       ttl: 100,
       storage: {
         type: 'redis',
         options: { client: redisClient, invalidation: true }
       },
-      onMiss () {
-        ++miss
-      },
+      onMiss,
+      onHit,
+      onError,
       policy: {
         Query: {
           get: {
-            references: async ({ arg }) => [`get:${arg.id}`]
-          },
-          search: true
-        },
-        Mutation: {
-          set: {
-            invalidate: async () => ['foo']
-          }
-        }
-      }
-    })
-    // Cache the follwoing
-    await request({ app, query: '{ get(id: 11) }' })
-    t.equal(miss, 1)
-    await request({ app, query: '{ get(id: 12) }' })
-    t.equal(miss, 2)
-    await request({ app, query: '{ search(id: 11) }' })
-    t.equal(miss, 3)
-    // Request a mutation
-    await request({ app, query: 'mutation { set(id: 11) }' })
-    t.equal(miss, 3)
-    // 'get:11' should be still in cache
-    await request({ app, query: '{ get(id: 11) }' })
-    t.equal(miss, 3)
-    await request({ app, query: '{ search(id: 11) }' })
-    t.equal(miss, 3)
-    await request({ app, query: '{ get(id: 12) }' })
-    t.equal(miss, 3)
-  })
-
-  test('should invalidate more than one reference at once', async t => {
-    // Setup Fastify and Mercurius
-    const app = fastify()
-    t.teardown(app.close.bind(app))
-    app.register(mercurius, { schema, resolvers })
-    // Setup Cache
-    let miss = 0
-    app.register(cache, {
-      ttl: 100,
-      storage: {
-        type: 'redis',
-        options: { client: redisClient, invalidation: true }
-      },
-      onMiss () {
-        ++miss
-      },
-      policy: {
-        Query: {
-          get: {
-            references: async ({ arg }) => [`get:${arg.id}`, 'gets']
-          },
-          search: true
-        },
-        Mutation: {
-          set: {
-            invalidate: async (_, arg) => [`get:${arg.id}`, 'gets']
-          }
-        }
-      }
-    })
-    // Cache the follwoing
-    await request({ app, query: '{ get(id: 11) }' })
-    t.equal(miss, 1)
-    await request({ app, query: '{ get(id: 12) }' })
-    t.equal(miss, 2)
-    await request({ app, query: '{ search(id: 11) }' })
-    t.equal(miss, 3)
-    // Request a mutation
-    await request({ app, query: 'mutation { set(id: 11) }' })
-    t.equal(miss, 3)
-    // All 'get' should not be present in cache anymore
-    await request({ app, query: '{ get(id: 11) }' })
-    t.equal(miss, 4)
-    await request({ app, query: '{ search(id: 11) }' })
-    t.equal(miss, 4)
-    await request({ app, query: '{ get(id: 12) }' })
-    t.equal(miss, 5)
-  })
-
-  test('should remove storage keys by references, but not the ones still alive', async t => {
-    // Setup Fastify and Mercurius
-    const app = fastify()
-    t.teardown(app.close.bind(app))
-    app.register(mercurius, { schema, resolvers })
-    // Setup Cache
-    let failHit = false
-    app.register(cache, {
-      ttl: 100,
-      storage: {
-        type: 'redis',
-        options: { client: redisClient, invalidation: true }
-      },
-      onHit () {
-        if (failHit) t.fail()
-      },
-      policy: {
-        Query: {
-          get: {
-            references: async ({ arg }) => [`get:${arg.id}`, 'gets']
+            references: ({ arg }) => [`get:${arg.id}`, 'gets']
           },
           search: {
-            references: async ({ arg }) => [`search:${arg.id}`, 'searchs']
+            references: ({ arg }) => [`search:${arg.id}`]
           }
         },
         Mutation: {
           set: {
-            invalidate: async () => ['gets']
+            invalidate: invalidate || ((_, arg) => [`get:${arg.id}`, 'gets'])
           }
         }
       }
+    })
+    return app
+  }
+
+  test('should remove storage keys by references', async tap => {
+    // Setup Fastify and Mercurius
+    let miss = 0
+    const app = setupServer({
+      onMiss: () => ++miss,
+      invalidate: (_, arg) => [`get:${arg.id}`],
+      tap
+    })
+    // Cache the follwoing
+    await request({ app, query: '{ get(id: 11) }' })
+    tap.equal(miss, 1)
+    await request({ app, query: '{ get(id: 12) }' })
+    tap.equal(miss, 2)
+    await request({ app, query: '{ search(id: 11) }' })
+    tap.equal(miss, 3)
+    // Request a mutation
+    await request({ app, query: 'mutation { set(id: 11) }' })
+    tap.equal(miss, 3)
+    // 'get:11' should not be present in cache anymore
+    await request({ app, query: '{ get(id: 11) }' })
+    tap.equal(miss, 4)
+    await request({ app, query: '{ search(id: 11) }' })
+    tap.equal(miss, 4)
+    await request({ app, query: '{ get(id: 12) }' })
+    tap.equal(miss, 4)
+  })
+
+  test('should not remove storage key by not existing reference', async tap => {
+    // Setup Fastify and Mercurius
+    let miss = 0
+    const app = setupServer({
+      onMiss: () => ++miss,
+      invalidate: () => ['foo'],
+      tap
+    })
+    // Cache the follwoing
+    await request({ app, query: '{ get(id: 11) }' })
+    tap.equal(miss, 1)
+    await request({ app, query: '{ get(id: 12) }' })
+    tap.equal(miss, 2)
+    await request({ app, query: '{ search(id: 11) }' })
+    tap.equal(miss, 3)
+    // Request a mutation
+    await request({ app, query: 'mutation { set(id: 11) }' })
+    tap.equal(miss, 3)
+    // 'get:11' should be still in cache
+    await request({ app, query: '{ get(id: 11) }' })
+    tap.equal(miss, 3)
+    await request({ app, query: '{ search(id: 11) }' })
+    tap.equal(miss, 3)
+    await request({ app, query: '{ get(id: 12) }' })
+    tap.equal(miss, 3)
+  })
+
+  test('should invalidate more than one reference at once', async tap => {
+    // Setup Fastify and Mercurius
+    let miss = 0
+    const app = setupServer({
+      onMiss: () => ++miss,
+      tap
+    })
+    // Cache the follwoing
+    await request({ app, query: '{ get(id: 11) }' })
+    tap.equal(miss, 1)
+    await request({ app, query: '{ get(id: 12) }' })
+    tap.equal(miss, 2)
+    await request({ app, query: '{ search(id: 11) }' })
+    tap.equal(miss, 3)
+    // Request a mutation
+    await request({ app, query: 'mutation { set(id: 11) }' })
+    tap.equal(miss, 3)
+    // All 'get' should not be present in cache anymore
+    await request({ app, query: '{ get(id: 11) }' })
+    tap.equal(miss, 4)
+    await request({ app, query: '{ search(id: 11) }' })
+    tap.equal(miss, 4)
+    await request({ app, query: '{ get(id: 12) }' })
+    tap.equal(miss, 5)
+  })
+
+  test('should remove storage keys by references, but not the ones still alive', async tap => {
+    // Setup Fastify and Mercurius
+    let failHit = false
+    const app = setupServer({
+      onHit () {
+        if (failHit) tap.fail()
+      },
+      tap
     })
     // Run the request and cache it
     await request({ app, query: '{ get(id: 11) }' })
-    t.equal(
+    tap.equal(
       await redisClient.get((await redisClient.smembers('r:get:11'))[0]),
       '"get 11"'
     )
     await request({ app, query: '{ get(id: 12) }' })
-    t.equal(
+    tap.equal(
       await redisClient.get((await redisClient.smembers('r:get:12'))[0]),
       '"get 12"'
     )
     await request({ app, query: '{ search(id: 11) }' })
-    t.equal(
+    tap.equal(
       await redisClient.get((await redisClient.smembers('r:search:11'))[0]),
       '"search 11"'
     )
     // Request a mutation, invalidate 'gets'
     await request({ app, query: 'mutation { set(id: 11) }' })
     // Check the references of 'searchs', should still be there
-    t.equal(
+    tap.equal(
       await redisClient.get((await redisClient.smembers('r:search:11'))[0]),
       '"search 11"'
     )
@@ -251,38 +193,19 @@ test('redis invalidation', async () => {
     await request({ app, query: '{ get(id: 11) }' })
   })
 
-  test('should not throw on invalidation error', async t => {
-    t.plan(3)
+  test('should not throw on invalidation error', async tap => {
+    tap.plan(3)
     // Setup Fastify and Mercurius
-    const app = fastify()
-    t.teardown(app.close.bind(app))
-    app.register(mercurius, { schema, resolvers })
-    // Setup Cache
-    app.register(cache, {
-      ttl: 100,
-      storage: {
-        type: 'redis',
-        options: { client: redisClient, invalidation: true }
+    const app = setupServer({
+      invalidate () {
+        throw new Error('Kaboom')
       },
       onError (type, fieldName, error) {
-        t.equal(type, 'Mutation')
-        t.equal(fieldName, 'set')
-        t.equal(error.message, 'Kaboom')
+        tap.equal(type, 'Mutation')
+        tap.equal(fieldName, 'set')
+        tap.equal(error.message, 'Kaboom')
       },
-      policy: {
-        Query: {
-          get: {
-            references: async ({ arg }) => [`get:${arg.id}`, 'gets']
-          }
-        },
-        Mutation: {
-          set: {
-            invalidate: async () => {
-              throw new Error('Kaboom')
-            }
-          }
-        }
-      }
+      tap
     })
     // Run the request and cache it
     await request({ app, query: '{ get(id: 11) }' })
