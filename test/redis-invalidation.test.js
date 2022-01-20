b20a328293b1df2a40d81605b41ec75c8d74acf3
Jhon Rocha <jhmrocha@gmail.com>
2022-01-20 08:45:06 -0300

    test: including some more tests

--- test/redis-invalidation.test.js
+++ test/redis-invalidation.test.js
@@ -4,11 +4,9 @@ const { test, before, teardown, beforeEach } = require('tap')
 const fastify = require('fastify')
 const mercurius = require('mercurius')
 const cache = require('..')
-const Redis = require('ioredis-mock')
+const Redis = require('ioredis')
 
 const redisClient = new Redis()
-
-const defaultPost = { method: 'POST', url: '/graphql' }
 
 before(async () => {
   await redisClient.flushall()
@@ -23,13 +21,8 @@ beforeEach(async () => {
 })
 
 test('redis invalidation', async () => {
-  test('should remove storage keys by references', async t => {
-    t.plan(7)
-
-    const app = fastify()
-    t.teardown(app.close.bind(app))
-
-    const schema = `
+  const defaultPost = { method: 'POST', url: '/graphql' }
+  const defaultSchema = `
     type Query {
       get (id: Int): String
       search (id: Int): String
@@ -38,24 +31,32 @@ test('redis invalidation', async () => {
       set (id: Int): String
     }
   `
-
-    const resolvers = {
-      Query: {
-        async get (_, { id }) {
-          return 'get ' + id
-        },
-        async search (_, { id }) {
-          return 'search ' + id
-        }
+  const defaultResolvers = {
+    Query: {
+      async get (_, { id }) {
+        return 'get ' + id
       },
-      Mutation: {
-        async set (_, { id }) {
-          return 'set ' + id
-        }
+      async search (_, { id }) {
+        return 'search ' + id
+      }
+    },
+    Mutation: {
+      async set (_, { id }) {
+        return 'set ' + id
       }
     }
+  }
 
-    app.register(mercurius, { schema, resolvers })
+  test('should remove storage keys by references', async t => {
+    t.plan(7)
+
+    const app = fastify()
+    t.teardown(app.close.bind(app))
+
+    app.register(mercurius, {
+      schema: defaultSchema,
+      resolvers: defaultResolvers
+    })
 
     let miss = 0
     app.register(cache, {
@@ -64,7 +65,7 @@ test('redis invalidation', async () => {
         type: 'redis',
         options: { client: redisClient, invalidation: true }
       },
-      onMiss (type, name) {
+      onMiss () {
         ++miss
       },
       policy: {
@@ -76,7 +77,7 @@ test('redis invalidation', async () => {
         },
         Mutation: {
           set: {
-            invalidate: async (self, arg) => [`get:${arg.id}`]
+            invalidate: async (_, arg) => [`get:${arg.id}`]
           }
         }
       }
@@ -109,33 +110,10 @@ test('redis invalidation', async () => {
     const app = fastify()
     t.teardown(app.close.bind(app))
 
-    const schema = `
-    type Query {
-      get (id: Int): String
-      search (id: Int): String
-    }
-    type Mutation {
-      set (id: Int): String
-    }
-  `
-
-    const resolvers = {
-      Query: {
-        async get (_, { id }) {
-          return 'get ' + id
-        },
-        async search (_, { id }) {
-          return 'search ' + id
-        }
-      },
-      Mutation: {
-        async set (_, { id }) {
-          return 'set ' + id
-        }
-      }
-    }
-
-    app.register(mercurius, { schema, resolvers })
+    app.register(mercurius, {
+      schema: defaultSchema,
+      resolvers: defaultResolvers
+    })
 
     let miss = 0
     app.register(cache, {
@@ -144,7 +122,7 @@ test('redis invalidation', async () => {
         type: 'redis',
         options: { client: redisClient, invalidation: true }
       },
-      onMiss (type, name) {
+      onMiss () {
         ++miss
       },
       policy: {
@@ -156,7 +134,7 @@ test('redis invalidation', async () => {
         },
         Mutation: {
           set: {
-            invalidate: async (self, arg) => ['foo']
+            invalidate: async () => ['foo']
           }
         }
       }
@@ -189,33 +167,10 @@ test('redis invalidation', async () => {
     const app = fastify()
     t.teardown(app.close.bind(app))
 
-    const schema = `
-    type Query {
-      get (id: Int): String
-      search (id: Int): String
-    }
-    type Mutation {
-      set (id: Int): String
-    }
-  `
-
-    const resolvers = {
-      Query: {
-        async get (_, { id }) {
-          return 'get ' + id
-        },
-        async search (_, { id }) {
-          return 'search ' + id
-        }
-      },
-      Mutation: {
-        async set (_, { id }) {
-          return 'set ' + id
-        }
-      }
-    }
-
-    app.register(mercurius, { schema, resolvers })
+    app.register(mercurius, {
+      schema: defaultSchema,
+      resolvers: defaultResolvers
+    })
 
     let miss = 0
     app.register(cache, {
@@ -224,7 +179,7 @@ test('redis invalidation', async () => {
         type: 'redis',
         options: { client: redisClient, invalidation: true }
       },
-      onMiss (type, name) {
+      onMiss () {
         ++miss
       },
       policy: {
@@ -236,7 +191,7 @@ test('redis invalidation', async () => {
         },
         Mutation: {
           set: {
-            invalidate: async (self, arg) => [`get:${arg.id}`, 'gets']
+            invalidate: async (_, arg) => [`get:${arg.id}`, 'gets']
           }
         }
       }
@@ -261,5 +216,55 @@ test('redis invalidation', async () => {
     t.equal(miss, 4)
     await app.inject({ ...defaultPost, body: { query: '{ get(id: 12) }' } })
     t.equal(miss, 5)
+  })
+
+  test('should remove storage keys by references, but not the ones still alive', async t => {
+    t.plan(2)
+
+    const app = fastify()
+    t.teardown(app.close.bind(app))
+    app.register(mercurius, {
+      schema: defaultSchema,
+      resolvers: defaultResolvers
+    })
+
+    app.register(cache, {
+      ttl: 1000,
+      storage: {
+        type: 'redis',
+        options: { client: redisClient, invalidation: true }
+      },
+      policy: {
+        Query: {
+          get: {
+            references: async ({ arg }) => [`get:${arg.id}`, 'gets']
+          },
+          search: {
+            references: async ({ arg }) => [`search:${arg.id}`, 'searchs']
+          }
+        },
+        Mutation: {
+          set: {
+            invalidate: async () => ['gets']
+          }
+        }
+      }
+    })
+    // Cache the follwoing
+    await app.inject({ ...defaultPost, body: { query: '{ get(id: 11) }' } })
+    await app.inject({ ...defaultPost, body: { query: '{ get(id: 12) }' } })
+    await app.inject({ ...defaultPost, body: { query: '{ search(id: 11) }' } })
+
+    // Request a mutation, invalidate 'gets'
+    await app.inject({
+      ...defaultPost,
+      body: { query: 'mutation { set(id: 11) }' }
+    })
+
+    // Check the references of 'searchs'
+    const results = await redisClient.smembers('r:searchs')
+    t.equal(results.length, 1)
+    // Check the content cached
+    t.equal(await redisClient.get(results[0]), '"search 11"')
   })
 })
