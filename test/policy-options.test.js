03a263a046c3bcf27fde11bdcdef39833950ea23
Simone Sanfratello <simone.sanfratello@nearform.com>
2022-02-23 14:40:11 +0100

    fix: test

--- test/policy-options.test.js
+++ test/policy-options.test.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const { test } = require('tap')
+const { test, teardown } = require('tap')
 const fastify = require('fastify')
 const mercurius = require('mercurius')
 const FakeTimers = require('@sinonjs/fake-timers')
@@ -8,6 +8,12 @@ const Redis = require('ioredis')
 const cache = require('..')
 
 const { request } = require('./helper')
+
+const redisClient = new Redis()
+
+teardown(async () => {
+  await redisClient.quit()
+})
 
 test('cache different policies with different options / ttl', async ({ equal, teardown }) => {
   const app = fastify()
@@ -295,14 +301,16 @@ test('cache per user using extendKey option', async ({ equal, same, teardown }) 
   equal(hits, 6)
 })
 
-test('should be able to use a custom key function, without fields', async t => {
-  const redisClient = new Redis()
-  await redisClient.flushall()
+test('custom key', async t => {
+  t.beforeEach(async () => {
+    await redisClient.flushall()
+  })
 
-  const app = fastify()
-  t.teardown(async () => Promise.all([app.close.bind(app), redisClient.quit()]))
+  t.test('should be able to use a custom key function, without fields', async t => {
+    const app = fastify()
+    t.teardown(app.close.bind(app))
 
-  const schema = `
+    const schema = `
     type Query {
       add(x: Int, y: Int): Int
       sub(x: Int, y: Int): Int
@@ -310,46 +318,43 @@ test('should be able to use a custom key function, without fields', async t => {
     }
     `
 
-  const resolvers = {
-    Query: {
-      async add (_, { x, y }) { return x + y },
-      async sub (_, { x, y }) { return x - y },
-      async mul (_, { x, y }) { return x * y }
-    }
-  }
-
-  app.register(mercurius, { schema, resolvers })
-
-  app.register(cache, {
-    ttl: 9,
-    storage: { type: 'redis', options: { client: redisClient } },
-    policy: {
+    const resolvers = {
       Query: {
-        add: { key ({ self, arg, info, ctx, fields }) { return `${arg.x}+${arg.y}` } },
-        sub: { key ({ self, arg, info, ctx, fields }) { return `${arg.x}-${arg.y}` } },
-        mul: { key ({ self, arg, info, ctx, fields }) { return `${arg.x}*${arg.y}` } }
+        async add (_, { x, y }) { return x + y },
+        async sub (_, { x, y }) { return x - y },
+        async mul (_, { x, y }) { return x * y }
       }
     }
+
+    app.register(mercurius, { schema, resolvers })
+
+    app.register(cache, {
+      ttl: 9,
+      storage: { type: 'redis', options: { client: redisClient } },
+      policy: {
+        Query: {
+          add: { key ({ self, arg, info, ctx, fields }) { return `${arg.x}+${arg.y}` } },
+          sub: { key ({ self, arg, info, ctx, fields }) { return `${arg.x}-${arg.y}` } },
+          mul: { key ({ self, arg, info, ctx, fields }) { return `${arg.x}*${arg.y}` } }
+        }
+      }
+    })
+
+    await request({ app, query: '{ add(x: 1, y: 1) }' })
+    t.equal(await redisClient.get('Query.add~1+1'), '2')
+
+    await request({ app, query: '{ sub(x: 2, y: 2) }' })
+    t.equal(await redisClient.get('Query.sub~2-2'), '0')
+
+    await request({ app, query: '{ mul(x: 3, y: 3) }' })
+    t.equal(await redisClient.get('Query.mul~3*3'), '9')
   })
 
-  await request({ app, query: '{ add(x: 1, y: 1) }' })
-  t.equal(await redisClient.get('Query.add~1+1'), '2')
+  t.test('should be able to use a custom key function, with fields without selection', async t => {
+    const app = fastify()
+    t.teardown(app.close.bind(app))
 
-  await request({ app, query: '{ sub(x: 2, y: 2) }' })
-  t.equal(await redisClient.get('Query.sub~2-2'), '0')
-
-  await request({ app, query: '{ mul(x: 3, y: 3) }' })
-  t.equal(await redisClient.get('Query.mul~3*3'), '9')
-})
-
-test('should be able to use a custom key function, with fields without selection', async t => {
-  const redisClient = new Redis()
-  await redisClient.flushall()
-
-  const app = fastify()
-  t.teardown(async () => Promise.all([app.close.bind(app), redisClient.quit()]))
-
-  const schema = `
+    const schema = `
   type Query {
     getUser (id: ID!): User
     getUsers (name: String, lastName: String): [User]
@@ -362,183 +367,180 @@ test('should be able to use a custom key function, with fields without selection
   }
 `
 
-  const users = {
-    a1: { name: 'Angus', lastName: 'Young' },
-    b2: { name: 'Phil', lastName: 'Rudd' },
-    c3: { name: 'Cliff', lastName: 'Williams' },
-    d4: { name: 'Brian', lastName: 'Johnson' },
-    e5: { name: 'Stevie', lastName: 'Young' }
-  }
-
-  const resolvers = {
-    Query: {
-      async getUser (_, { id }) { return users[id] ? { id, ...users[id] } : null },
-      async getUsers (_, { name, lastName }) {
-        const id = Object.keys(users).find(key => {
-          const user = users[key]
-          if (name && user.name !== name) return false
-          if (lastName && user.lastName !== lastName) return false
-          return true
-        })
-        return id ? [{ id, ...users[id] }] : []
-      }
+    const users = {
+      a1: { name: 'Angus', lastName: 'Young' },
+      b2: { name: 'Phil', lastName: 'Rudd' },
+      c3: { name: 'Cliff', lastName: 'Williams' },
+      d4: { name: 'Brian', lastName: 'Johnson' },
+      e5: { name: 'Stevie', lastName: 'Young' }
     }
-  }
 
-  app.register(mercurius, { schema, resolvers })
-
-  const hits = { getUser: 0, getUsers: 0 }
-  app.register(cache, {
-    ttl: 3,
-    storage: {
-      type: 'redis',
-      options: { client: redisClient }
-    },
-    onHit (type, name) { hits[name]++ },
-    policy: {
+    const resolvers = {
       Query: {
-        getUser: { key ({ self, arg, info, ctx, fields }) { return `${arg.id}` } },
-        getUsers: { key ({ self, arg, info, ctx, fields }) { return `${arg.name || '*'},${arg.lastName || '*'}` } }
+        async getUser (_, { id }) { return users[id] ? { id, ...users[id] } : null },
+        async getUsers (_, { name, lastName }) {
+          const id = Object.keys(users).find(key => {
+            const user = users[key]
+            if (name && user.name !== name) return false
+            if (lastName && user.lastName !== lastName) return false
+            return true
+          })
+          return id ? [{ id, ...users[id] }] : []
+        }
       }
     }
-  })
 
-  // use key and store in cache the user
-  t.same(await request({ app, query: '{ getUser(id: "a1") { name, lastName} }' }), {
-    data: { getUser: { name: 'Angus', lastName: 'Young' } }
-  })
-  t.equal(await redisClient.get('Query.getUser~a1'), JSON.stringify({ id: 'a1', lastName: 'Young', name: 'Angus' }))
+    app.register(mercurius, { schema, resolvers })
 
-  // use key and get the user from cache
-  t.same(await request({ app, query: '{ getUser(id: "a1") { id } }' }), {
-    data: { getUser: { id: 'a1' } }
-  })
-  t.equal(hits.getUser, 1)
-
-  // query users
-  t.same(await request({ app, query: '{ getUsers(name: "Brian") { id, name, lastName} }' }), {
-    data: { getUsers: [{ id: 'd4', name: 'Brian', lastName: 'Johnson' }] }
-  })
-  t.equal(await redisClient.get('Query.getUsers~Brian,*'), JSON.stringify([{ id: 'd4', lastName: 'Johnson', name: 'Brian' }]))
-
-  t.same(await request({ app, query: '{ getUsers(name: "Brian") { name } }' }), {
-    data: { getUsers: [{ name: 'Brian' }] }
-  })
-  t.equal(hits.getUsers, 1)
-})
-
-test('should be able to use a custom key function, with fields selection', async t => {
-  function selectedFields (info) {
-    const fields = []
-    for (let i = 0; i < info.fieldNodes.length; i++) {
-      const node = info.fieldNodes[i]
-      if (!node.selectionSet) {
-        continue
-      }
-      for (let j = 0; j < node.selectionSet.selections.length; j++) {
-        fields.push(node.selectionSet.selections[j].name.value)
-      }
-    }
-    fields.sort()
-    return fields
-  }
-
-  const redisClient = new Redis()
-  await redisClient.flushall()
-
-  const app = fastify()
-  t.teardown(async () => Promise.all([app.close.bind(app), redisClient.quit()]))
-
-  const schema = `
-  type Query {
-    getUser (id: ID!): User
-    getUsers (name: String, lastName: String): [User]
-  }
-
-  type User {
-    id: ID!
-    name: String 
-    lastName: String
-  }
-`
-
-  const users = {
-    a1: { name: 'Angus', lastName: 'Young' },
-    b2: { name: 'Phil', lastName: 'Rudd' },
-    c3: { name: 'Cliff', lastName: 'Williams' },
-    d4: { name: 'Brian', lastName: 'Johnson' },
-    e5: { name: 'Stevie', lastName: 'Young' }
-  }
-
-  const resolvers = {
-    Query: {
-      async getUser (_, { id }, context, info) {
-        if (!users[id]) { return null }
-        const fields = selectedFields(info)
-        const user = fields.reduce((user, field) => ({ ...user, [field]: users[id][field] }), {})
-        if (fields.includes('id')) { user.id = id }
-        return user
+    const hits = { getUser: 0, getUsers: 0 }
+    app.register(cache, {
+      ttl: 3,
+      storage: {
+        type: 'redis',
+        options: { client: redisClient }
       },
-      async getUsers (_, { name, lastName }, context, info) {
-        const ids = Object.keys(users).filter(key => {
-          const user = users[key]
-          if (name && user.name !== name) return false
-          if (lastName && user.lastName !== lastName) return false
-          return true
-        })
-        const fields = selectedFields(info)
-        const withId = fields.includes('id')
-        return ids.map(id => {
-          const user = fields.reduce((user, field) => ({ ...user, [field]: users[id][field] }), {})
-          if (withId) { user.id = id }
-          return user
-        })
+      onHit (type, name) { hits[name]++ },
+      policy: {
+        Query: {
+          getUser: { key ({ self, arg, info, ctx, fields }) { return `${arg.id}` } },
+          getUsers: { key ({ self, arg, info, ctx, fields }) { return `${arg.name || '*'},${arg.lastName || '*'}` } }
+        }
       }
+    })
+
+    // use key and store in cache the user
+    t.same(await request({ app, query: '{ getUser(id: "a1") { name, lastName} }' }), {
+      data: { getUser: { name: 'Angus', lastName: 'Young' } }
+    })
+    t.equal(await redisClient.get('Query.getUser~a1'), JSON.stringify({ id: 'a1', lastName: 'Young', name: 'Angus' }))
+
+    // use key and get the user from cache
+    t.same(await request({ app, query: '{ getUser(id: "a1") { id } }' }), {
+      data: { getUser: { id: 'a1' } }
+    })
+    t.equal(hits.getUser, 1)
+
+    // query users
+    t.same(await request({ app, query: '{ getUsers(name: "Brian") { id, name, lastName} }' }), {
+      data: { getUsers: [{ id: 'd4', name: 'Brian', lastName: 'Johnson' }] }
+    })
+    t.equal(await redisClient.get('Query.getUsers~Brian,*'), JSON.stringify([{ id: 'd4', lastName: 'Johnson', name: 'Brian' }]))
+
+    t.same(await request({ app, query: '{ getUsers(name: "Brian") { name } }' }), {
+      data: { getUsers: [{ name: 'Brian' }] }
+    })
+    t.equal(hits.getUsers, 1)
+  })
+
+  test('should be able to use a custom key function, with fields selection', async t => {
+    function selectedFields (info) {
+      const fields = []
+      for (let i = 0; i < info.fieldNodes.length; i++) {
+        const node = info.fieldNodes[i]
+        if (!node.selectionSet) {
+          continue
+        }
+        for (let j = 0; j < node.selectionSet.selections.length; j++) {
+          fields.push(node.selectionSet.selections[j].name.value)
+        }
+      }
+      fields.sort()
+      return fields
     }
-  }
+    const app = fastify()
+    t.teardown(app.close.bind(app))
 
-  app.register(mercurius, { schema, resolvers })
+    const schema = `
+      type Query {
+        getUser (id: ID!): User
+        getUsers (name: String, lastName: String): [User]
+      }
 
-  let hits = 0
-  app.register(cache, {
-    ttl: 3,
-    storage: {
-      type: 'redis',
-      options: { client: redisClient }
-    },
-    onHit (type, name) { hits++ },
-    policy: {
+      type User {
+        id: ID!
+        name: String 
+        lastName: String
+      }
+    `
+
+    const users = {
+      a1: { name: 'Angus', lastName: 'Young' },
+      b2: { name: 'Phil', lastName: 'Rudd' },
+      c3: { name: 'Cliff', lastName: 'Williams' },
+      d4: { name: 'Brian', lastName: 'Johnson' },
+      e5: { name: 'Stevie', lastName: 'Young' }
+    }
+
+    const resolvers = {
       Query: {
-        getUser: { key ({ self, arg, info, ctx, fields }) { return `${arg.id}:${fields.join()}` } },
-        getUsers: { key ({ self, arg, info, ctx, fields }) { return `${arg.name || '*'},${arg.lastName || '*'}:${fields.join()}` } }
+        async getUser (_, { id }, context, info) {
+          if (!users[id]) { return null }
+          const fields = selectedFields(info)
+          const user = fields.reduce((user, field) => ({ ...user, [field]: users[id][field] }), {})
+          if (fields.includes('id')) { user.id = id }
+          return user
+        },
+        async getUsers (_, { name, lastName }, context, info) {
+          const ids = Object.keys(users).filter(key => {
+            const user = users[key]
+            if (name && user.name !== name) return false
+            if (lastName && user.lastName !== lastName) return false
+            return true
+          })
+          const fields = selectedFields(info)
+          const withId = fields.includes('id')
+          return ids.map(id => {
+            const user = fields.reduce((user, field) => ({ ...user, [field]: users[id][field] }), {})
+            if (withId) { user.id = id }
+            return user
+          })
+        }
       }
     }
-  })
 
-  // use key and store in cache the user
-  t.same(await request({ app, query: '{ getUser(id: "a1") { name, lastName} }' }), {
-    data: { getUser: { name: 'Angus', lastName: 'Young' } }
-  })
-  t.equal(await redisClient.get('Query.getUser~a1:lastName,name'), JSON.stringify({ lastName: 'Young', name: 'Angus' }))
+    app.register(mercurius, { schema, resolvers })
 
-  // use key and get the user from cache
-  t.same(await request({ app, query: '{ getUser(id: "a1") { id } }' }), {
-    data: { getUser: { id: 'a1' } }
-  })
-  t.equal(await redisClient.get('Query.getUser~a1:id'), JSON.stringify({ id: 'a1' }))
+    let hits = 0
+    app.register(cache, {
+      ttl: 3,
+      storage: {
+        type: 'redis',
+        options: { client: redisClient }
+      },
+      onHit (type, name) { hits++ },
+      policy: {
+        Query: {
+          getUser: { key ({ self, arg, info, ctx, fields }) { return `${arg.id}:${fields.join()}` } },
+          getUsers: { key ({ self, arg, info, ctx, fields }) { return `${arg.name || '*'},${arg.lastName || '*'}:${fields.join()}` } }
+        }
+      }
+    })
 
-  // query users
-  t.same(await request({ app, query: '{ getUsers(lastName: "Young") { id, name, lastName} }' }), {
-    data: { getUsers: [{ id: 'a1', name: 'Angus', lastName: 'Young' }, { id: 'e5', name: 'Stevie', lastName: 'Young' }] }
-  })
-  t.equal(await redisClient.get('Query.getUsers~*,Young:id,lastName,name'), JSON.stringify([{ id: 'a1', lastName: 'Young', name: 'Angus' }, { id: 'e5', lastName: 'Young', name: 'Stevie' }]))
+    // use key and store in cache the user
+    t.same(await request({ app, query: '{ getUser(id: "a1") { name, lastName} }' }), {
+      data: { getUser: { name: 'Angus', lastName: 'Young' } }
+    })
+    t.equal(await redisClient.get('Query.getUser~a1:lastName,name'), JSON.stringify({ lastName: 'Young', name: 'Angus' }))
 
-  // query users different fields
-  t.same(await request({ app, query: '{ getUsers(lastName: "Young") { name } }' }), {
-    data: { getUsers: [{ name: 'Angus' }, { name: 'Stevie' }] }
-  })
-  t.equal(await redisClient.get('Query.getUsers~*,Young:name'), JSON.stringify([{ name: 'Angus' }, { name: 'Stevie' }]))
+    // use key and get the user from cache
+    t.same(await request({ app, query: '{ getUser(id: "a1") { id } }' }), {
+      data: { getUser: { id: 'a1' } }
+    })
+    t.equal(await redisClient.get('Query.getUser~a1:id'), JSON.stringify({ id: 'a1' }))
 
-  // never used the cache
-  t.equal(hits, 0)
+    // query users
+    t.same(await request({ app, query: '{ getUsers(lastName: "Young") { id, name, lastName} }' }), {
+      data: { getUsers: [{ id: 'a1', name: 'Angus', lastName: 'Young' }, { id: 'e5', name: 'Stevie', lastName: 'Young' }] }
+    })
+    t.equal(await redisClient.get('Query.getUsers~*,Young:id,lastName,name'), JSON.stringify([{ id: 'a1', lastName: 'Young', name: 'Angus' }, { id: 'e5', lastName: 'Young', name: 'Stevie' }]))
+
+    // query users different fields
+    t.same(await request({ app, query: '{ getUsers(lastName: "Young") { name } }' }), {
+      data: { getUsers: [{ name: 'Angus' }, { name: 'Stevie' }] }
+    })
+    t.equal(await redisClient.get('Query.getUsers~*,Young:name'), JSON.stringify([{ name: 'Angus' }, { name: 'Stevie' }]))
+
+    // never used the cache
+    t.equal(hits, 0)
+  })
 })
